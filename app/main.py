b1d9a4ca54a55c116b698e4df2bdf54472f0c6d2
benitalker <benitalker00@gmail.com>
2024-11-28 11:01:54 +0200

    start of project

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+from app.routs.phone_rout import phone_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(phone_blueprint,url_prefix="")
+
+if __name__ == '__main__':
+    app.run()
